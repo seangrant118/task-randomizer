f8567c1db850d4b2da63cee15c89edf3ee101d8c
Sean Grant <seangrant118@gmail.com>
2021-02-02 10:44:49 -0500

    home skel

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <button className="home__generate">Get a random task</button>
+        <div className="home__currentTask">current task</div>
+        <button className="home__completeT">Complete Task</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;

c15d179936776d21bc52e43a42fb88ee6222e66e
Sean Grant <seangrant118@gmail.com>
2021-02-09 14:38:21 -0500

    header links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,35 @@
 import React from "react";
 import AddIcon from "@material-ui/icons/Add";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
       <div className="header__home">
-        <p>Randomize Task</p>
+        <Link to="/">
+          <p>Randomize Task</p>
+        </Link>
       </div>
       <div className="header__right">
-        <div className="header__option">
-          <p className="header__signin">Sign in</p>
-          <AccountCircleIcon />
-        </div>
-        <div className="header__option">
-          <p className="header__addTask">Create Task</p>
-          <AddIcon />
-        </div>
-        <div className="header__option">
-          <p className="header__taskList">My Tasks</p>
-        </div>
+        <Link to="/signin">
+          <div className="header__option">
+            <p className="header__signin">Sign in</p>
+            <AccountCircleIcon />
+          </div>
+        </Link>
+        <Link to="/createtask">
+          <div className="header__option">
+            <p className="header__addTask">Create Task</p>
+            <AddIcon />
+          </div>
+        </Link>
+        <Link to="/mytasks">
+          <div className="header__option">
+            <p className="header__taskList">My Tasks</p>
+          </div>
+        </Link>
       </div>
     </div>
   );

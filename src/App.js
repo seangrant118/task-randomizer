dd6729c9377db3c5dc0fbf5e02182fc4f8c25123
Sean Grant <seangrant118@gmail.com>
2021-02-09 14:34:05 -0500

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import "./App.css";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import SignIn from "./components/Signin/SignIn";
+import CreateTask from "./components/CreateTask/CreateTask";
+import MyTasks from "./components/MyTasks/MyTasks";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/createtask">
+            <CreateTask />
+          </Route>
+          <Route path="/mytasks">
+            <MyTasks />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

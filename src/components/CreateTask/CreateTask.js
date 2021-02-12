e57750851fa9c7b549579321db13da3821aec062
Sean Grant <seangrant118@gmail.com>
2021-02-12 13:58:46 -0500

    create min styling

--- src/components/CreateTask/CreateTask.js
+++ src/components/CreateTask/CreateTask.js
@@ -1,11 +1,16 @@
 import React from "react";
+import "./CreateTask.css";
 
 function CreateTask() {
   return (
-    <div>
-      <div>Create a new task</div>
-      <input type="text" />
-      <button>Create</button>
+    <div className="create">
+      <div className="create__container">
+        <div className="create__items">
+          <div className="create__title">Create a new task</div>
+          <textarea className="create__input" />
+        </div>
+        <button className="create__button">Create</button>
+      </div>
     </div>
   );
 }

be889e33caec9704e6050b6ba633ce7ba09fa364
Sean Grant <seangrant118@gmail.com>
2021-02-16 12:20:48 -0500

    auth redirect

--- src/components/Signin/SignIn.js
+++ src/components/Signin/SignIn.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { auth } from "../../firebase.js";
+import firebase from "firebase";
+import { Link, useHistory } from "react-router-dom";
 import "./SignIn.css";
 
 function SignIn() {
+  const history = useHistory();
+
+  const googleSignIn = (e) => {
+    e.preventDefault();
+
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        const credential = result.credential;
+        const token = credential.accessToken;
+        const user = result.user;
+        history.push("/");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const credential = error.credential;
+      });
+  };
+
   return (
     <div className="signin__container">
-      <button className="signin">Sign in With Google</button>
+      <button onClick={googleSignIn} className="signin">
+        Sign in With Google
+      </button>
     </div>
   );
 }

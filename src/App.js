bd525c8d3c808a93ec9ed8f51f95d8c04062495d
Sean Grant <seangrant118@gmail.com>
2021-02-16 14:40:05 -0500

    redicer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
---
+++ src/reducer/reducer.js
@@ -0,0 +1,10 @@
+export const initialState = {
+  basket: [],
+};
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};

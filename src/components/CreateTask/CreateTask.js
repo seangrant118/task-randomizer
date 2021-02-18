9638699b0680b758ca186b352763f265db016635
Sean Grant <seangrant118@gmail.com>
2021-02-18 11:58:33 -0500

    create task state

--- src/components/CreateTask/CreateTask.js
+++ src/components/CreateTask/CreateTask.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CreateTask.css";
 
 function CreateTask() {
+  const [task, createTask] = useState();
+
   return (
     <div className="create">
       <div className="create__container">
         <div className="create__items">
           <div className="create__title">Create a new task</div>
-          <textarea className="create__input" />
+          <textarea
+            className="create__input"
+            onChange={(e) => createTask(e.target.value)}
+          />
         </div>
         <button className="create__button">Create</button>
       </div>

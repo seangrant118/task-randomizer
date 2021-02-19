55ec91a856be99aeb068c52fdd7aebbffca26813
Sean Grant <seangrant118@gmail.com>
2021-02-19 15:42:47 -0500

    write task to my tasks

--- src/components/MyTasks/MyTasks.js
+++ src/components/MyTasks/MyTasks.js
@@ -1,16 +1,20 @@
 import React, { useEffect } from "react";
 import "./MyTasks.css";
 // import { db } from "../../firebase";
-// import { useStateValue } from "../../StateProvider";
+import { useStateValue } from "../../StateProvider";
 
 function MyTasks() {
-  // const [{ user }, dispatch] = useStateValue();
+  const [{ user, tasks }, dispatch] = useStateValue();
 
   return (
     <div className="tasks">
       <div className="tasks__container">
         <h2 className="tasks__title">My Tasks</h2>
-        <div className="task__items"></div>
+        <div className="task__items">
+          {tasks.map((task) => (
+            <div>{task}</div>
+          ))}
+        </div>
       </div>
     </div>
   );

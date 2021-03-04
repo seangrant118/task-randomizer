32e0403f1438f795faa322cce900658ba4af04fc
Sean Grant <seangrant118@gmail.com>
2021-03-04 13:16:22 -0500

    signin button

--- src/components/Signin/SignIn.js
+++ src/components/Signin/SignIn.js
@@ -31,6 +31,11 @@ function SignIn() {
   return (
     <div className="signin__container">
       <button onClick={googleSignIn} className="signin">
+        <img
+          className="google__logo"
+          alt="googlelogo"
+          src="https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip"
+        ></img>
         Sign in With Google
       </button>
     </div>
